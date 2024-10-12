83a73b0649e33aa9c0eb4dfef4c3d36af10db7b7
Md Mamun Hossain <gmmamun3004@gmail.com>
2024-10-12 10:09:54 +0600

    [Update] posts data ui update

--- src/components/posts/PostInfo.jsx
+++ src/components/posts/PostInfo.jsx
@@ -1,19 +1,27 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import { Button } from '../utilitys/Button';
 
-export const PostsInfo = ({postList}) => {
-  console.log(postList);
-  
+export const PostInfo = ({postList}) => {
+    console.log(postList);
+    
   return (
-    <>
-      <section>
-        <div className="grid grid-cols-1  gap-5 mt-5 max-w-screen-sm mx-auto">
-          <div className="border shadow rounded-md  p-8 ">
-            <h1>{postList?.title}</h1>
+    <div>
+      <div className="grid grid-cols-1  gap-5 mt-5 max-w-screen-sm mx-auto">
+        <div className="border shadow rounded-md  p-8 ">
+          <div className="flex flex-col gap-4">
+            <p className="text-2xl font-bold hover:underline duration-300 underline-offset-8">
+              {postList?.title}
+            </p>
+
+            <p className="text-sm text-gray-900 font-normal">
+              {postList?.body}
+            </p>
+            <Button>View user profile</Button>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </div>
   );
 }
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -4,13 +4,17 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Header } from "../shared/Header";
 import { MorePage } from "../utilitys/MorePage";
+import { PostInfo } from "./PostInfo";
 
 export const Posts = () => {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
       setPosts(response?.data);
+      setLoading(false)
+
 
     });
   }, []);
@@ -21,11 +25,16 @@ export const Posts = () => {
       <Header />
       {/* <Header search={search} searchHandler={searchHandler} /> */}
       <div className="mt-20 mb-8">
-        {
+        {loading ? (
+          <h1 className="text-2xl font-bold text-center text-red-700">
+            Wait, data loading...
+          </h1>
+        ) : (
           posts.map((post) => (
-            <div key={post.id}>{post?.title}</div>
+            // <div key={post.id}>{post?.title}</div>
+            <PostInfo key={post?.id} postList={post} />
           ))
-        }
+        )}
       </div>
       <MorePage />
     </>
